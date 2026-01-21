dcbda66cc547cdc9931f2ac625f49f5fd8f94517
HuongVT <134629397+HuongVT0503@users.noreply.github.com>
2026-01-21 20:21:13 +0700

    polyfill buffer for clientside hashing

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,12 @@
+import { Buffer } from 'buffer';
+// @ts-expect-error - polyfilling Buffer for browser
+globalThis.Buffer = Buffer;
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
